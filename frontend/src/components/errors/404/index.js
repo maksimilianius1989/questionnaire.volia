66f0e70f87db4e5381751cdc0ebbbaae2aaeff0c
Bukach_M <maksym.bukach@volia.com>
2019-10-04 14:13:57 +0300

    Create folder structure

---
+++ frontend/src/components/errors/404/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function () {
+    return (
+        <>
+            <h1>Error 404, page not found</h1>
+        </>
+    )
+}
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import Test from './test'
-import Mark from './mark'
+
+import Error from '~p/error404'
+import Mark from '~p/mark'
+import Test from '~p/test'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+// ReactDom.render(<Error />, document.querySelector("#app"))
 // ReactDom.render(<Mark />, document.querySelector("#app"))
 ReactDom.render(<Test />, document.querySelector("#app"))
